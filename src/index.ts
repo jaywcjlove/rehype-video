7c13f5fc9b96e261c6656ca5b3238a1351861aaf
jaywcjlove <398188662@qq.com>
2022-03-20 01:20:20 +0800

    feat: add custom title feature.

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,20 @@ export type RehypeVideoOptions = {
 }
 
 const properties = { muted: 'muted', controls: 'controls', style: 'max-height:640px;' };
+const queryStringToObject = (url: string) =>
+  [...new URLSearchParams(url.split('?!#')[1])].reduce(
+    (a: Record<string, string>, [k, v]) => ((a[k] = v), a),
+    {}
+  );
 
 function reElement(node: Element, details: boolean, href: string) {
-  const filename = href.split('/').pop();
+  const filename = href.split('/').pop()?.replace(/(\?|!|\#|$).+/, '');
   node.properties = { ...properties, src: href };
   node.tagName = 'video';
   node.children = [];
+  const { title = filename }= queryStringToObject(href);
   if (details) {
-    const reNode = detailsNode(filename);
+    const reNode = detailsNode(title);
     reNode.children.push({ ...node });
     node.children = reNode.children;
     node.tagName = reNode.tagName;
@@ -36,7 +42,7 @@ const RehypeVideo: Plugin<[RehypeVideoOptions?], Root> = (options) => {
   const { test = /\/(.*)(.mp4|.mov)$/, details = true } = options || {};
   return (tree) => {
     visit(tree, 'element', (node, index, parent) => {
-      const isChecked = (str: string) => test.test(str)
+      const isChecked = (str: string) => test.test(str.replace(/(\?|!|\#|$).+/g, ''))
       const child = node.children[0];
       const delimiter = /((?:https?:\/\/)(?:(?:[a-z0-9]?(?:[a-z0-9\-]{1,61}[a-z0-9])?\.[^\.|\s])+[a-z\.]*[a-z]+|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})(?::\d{1,5})*[a-z0-9.,_\/~#&=;%+?\-\\(\\)]*)/g;
       // const delimiter = /((?:https?:\/\/)?(?:(?:[a-z0-9]?(?:[a-z0-9\-]{1,61}[a-z0-9])?\.[^\.|\s])+[a-z\.]*[a-z]+|(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})(?::\d{1,5})*[a-z0-9.,_\/~#&=;%+?\-\\(\\)]*)/g;
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,6 +6,19 @@ import remarkParse from 'remark-parse';
 import stringify from 'rehype-stringify';
 import rehypeVideo from '../src';
 
+it('rehype-video title test case', () => {
+  const mrkStr = `https://github.com/002.mp4?!#title=rehype-video`;
+  const expected = `<details open class=\"octicon octicon-video\"><summary><svg aria-hidden height=\"16\" width=\"16\" viewBox=\"0 0 16 16\" version=\"1.1\" data-view-component class=\"octicon octicon-device-camera-video\"><path fill-rule=\"evenodd\" d=\"M16 3.75a.75.75 0 00-1.136-.643L11 5.425V4.75A1.75 1.75 0 009.25 3h-7.5A1.75 1.75 0 000 4.75v6.5C0 12.216.784 13 1.75 13h7.5A1.75 1.75 0 0011 11.25v-.675l3.864 2.318A.75.75 0 0016 12.25v-8.5zm-5 5.075l3.5 2.1v-5.85l-3.5 2.1v1.65zM9.5 6.75v-2a.25.25 0 00-.25-.25h-7.5a.25.25 0 00-.25.25v6.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-4.5z\"></path></svg><span aria-label=\"Video description rehype-video\">rehype-video</span><span class=\"dropdown-caret\"></span></summary><video muted controls style=\"max-height:640px;\" src=\"https://github.com/002.mp4?!#title=rehype-video\"></video></details>`
+  const htmlStr = unified()
+    .use(remarkParse)
+    .use(remark2rehype, { allowDangerousHtml: true })
+    .use(rehypeVideo, { })
+    .use(stringify)
+    .processSync(mrkStr)
+    .toString();
+    expect(htmlStr).toEqual(expected);
+});
+
 it('rehype-video 1 test case', () => {
   const mrkStr = `
   https://github.com/001.mp4 hi!
